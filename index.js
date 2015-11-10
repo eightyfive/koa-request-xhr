328a624d610d39dad2d0271101ed059c02921da3
Benoit Sagols <benoit.sagols@gmail.com>
2015-11-11 01:11:46 +0700

    Using recommended ctx.state instead

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = function xhr() {
   return function *xhr(next) {
-    this.request.xhr = (this.request.get('X-Requested-With') === 'XMLHttpRequest');
+    this.state.xhr = (this.request.get('X-Requested-With') === 'XMLHttpRequest');
     yield next;
   }
 };
